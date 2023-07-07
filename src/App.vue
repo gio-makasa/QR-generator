61c55cbe3fcb1dde40ba171740272b0d7e0c193f
gio <giorgimakasarashvili@gmail.com>
2023-07-07 15:32:54 +0400

    added design

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>QR Code Generator</h1>
+  <InputContainer />
+  <img src="./assets/tourists-at-victoria-falls.jpg" alt="image" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script setup lang="ts">
+import InputContainer from "./components/InputContainer.vue";
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 200px;
+  height: 200px;
+  margin-top: 10vh;
 }
 </style>
---
+++ src/components/InputContainer.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <input type="text" />
+    <input
+      type="file"
+      id="background"
+      accept="image/png, image/jpeg"
+    />
+    <button>GENERATE</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 40%;
+  height: 20vh;
+  padding: 1rem;
+  border-radius: 1rem;
+  box-shadow: 0 0 10px white;
+
+  input[type="text"] {
+    border-radius: 5rem;
+    padding: 0.5rem 1rem;
+    font-size: large;
+  }
+
+  button {
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    background-color: #1a1a1a;
+    color: white;
+    cursor: pointer;
+    transition: border-color 0.25s;
+
+    &:hover {
+      border-color: #646cff;
+    }
+
+    &:focus,
+    &:focus-visible {
+      outline: 4px auto -webkit-focus-ring-color;
+    }
+  }
+}
+</style>
